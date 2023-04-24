2c1469b8aede0d31d09625f7268c7def384c8f86
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-24 19:13:51 +0530

    Add images and update countries.js file

---
+++ src/components/Countries.js
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const Countries = () => {
+  const { countries, map } = useSelector((state) => state.countries);
+
+  let output = [];
+
+  if (map !== 'SELECT A CONTINENT') {
+    if (countries.length !== 0) {
+      output = countries.filter((item) => item.continent[0] === map);
+    }
+  }
+
+  return (
+    <ul className="countries-list" data-testid="countries-list">
+      {map === 'SELECT A CONTINENT'
+        ? <h2>No continent selected</h2>
+        : output.map((country) => (
+          <li className="countries-item" key={country.name.common}>
+            <Link to={`/countries/${country.name.common}`}>
+              <img
+                className="flag"
+                src={country.flags.png}
+                alt={`${country.name.common}'s Flag`}
+              />
+              <span className="bold">{country.name.common}</span>
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
+};
+export default Countries;
