d828c02fe903d2b90c226a8eee90eae5f2d622ee
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-26 21:55:45 +0530

    Set a default continent on the page

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -10,24 +10,25 @@ const Countries = () => {
     if (countries.length !== 0) {
       output = countries.filter((item) => item.continents[0] === map);
     }
+  } else {
+    // Display a default continent if no continent is selected
+    output = countries.filter((item) => item.continents[0] === 'Africa');
   }
 
   return (
     <ul className="countries-list" data-testid="countries-list">
-      {map === 'SELECT A CONTINENT'
-        ? <h2>No continent selected</h2>
-        : output.map((country) => (
-          <li className="countries-item" key={country.name.common}>
-            <Link to={`/countries/${country.name.common}`}>
-              <img
-                className="flag"
-                src={country.flags.png}
-                alt={`${country.name.common}'s Flag`}
-              />
-              <span className="bold">{country.name.common}</span>
-            </Link>
-          </li>
-        ))}
+      {output.map((country) => (
+        <li className="countries-item" key={country.name.common}>
+          <Link to={`/countries/${country.name.common}`}>
+            <img
+              className="flag"
+              src={country.flags.png}
+              alt={`${country.name.common}'s Flag`}
+            />
+            <span className="bold">{country.name.common}</span>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
